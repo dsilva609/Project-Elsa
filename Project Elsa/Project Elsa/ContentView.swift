d8f4d36838c738710cb1e050824b4e3ed120d30e
Dennis D Silva <dsilva609@gmail.com>
2021-02-03 22:47:50 -0500

    project: initial add

---
+++ Project Elsa/Project Elsa/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project Elsa
+//
+//  Created by Dennis Silva on 2/3/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Project Elsa/Project Elsa/Project_ElsaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_ElsaApp.swift
+//  Project Elsa
+//
+//  Created by Dennis Silva on 2/3/21.
+//
+
+import SwiftUI
+
+@main
+struct Project_ElsaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
